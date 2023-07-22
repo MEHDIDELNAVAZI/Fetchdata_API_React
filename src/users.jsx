d6caa956999ffb253fcf619cda6330f5d4c4f066
mehdi <delnavazi1029@gmail.com>
2023-07-22 20:32:04 +0330

    Initial project setup We install Bootstrap and axios for Using Api to fetch data from https://reqres.in

---
+++ src/users.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Users extends Component {
+  state = {};
+  render() {
+    return <div>Hey this is users from api !</div>;
+  }
+}
+
+export default Users;
